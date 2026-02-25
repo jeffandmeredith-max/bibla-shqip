eb716ad7ef9793c9ca4c599a6160053356c7461f
Daily Update <daily-update@github-actions>
2026-02-25 11:29:04 +0000

    chore: daily update 2026-02-25

--- src/data/february.js
+++ src/data/february.js
@@ -593,6 +593,27 @@ export const february = [
     "date": "25 Shkurt",
     "videoId": "zZfxfrpO_r4",
     "audioFile": "february-25.webm",
-    "readings": []
+    "readings": [
+      {
+        "title": "Eksodi 8",
+        "start": 0
+      },
+      {
+        "title": "Luka 11",
+        "start": 307
+      },
+      {
+        "title": "Jobi 25",
+        "start": 714
+      },
+      {
+        "title": "Jobi 26",
+        "start": 749
+      },
+      {
+        "title": "1 e Korintasve 12",
+        "start": 818
+      }
+    ]
   }
 ]
